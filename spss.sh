8387128b1cbd9e0e661601e1b38bc59a35a35b4b
al <45895462+JAlbertoGonzalez@users.noreply.github.com>
2022-03-13 19:51:53 +0100

    register mime type + icon

--- spss.sh
+++ spss.sh
@@ -21,9 +21,13 @@ fi
 
 DESKTOP_FILE=https://raw.githubusercontent.com/JAlbertoGonzalez/spss-linux-launcher/main/spss.desktop
 ICON_FILE=https://raw.githubusercontent.com/JAlbertoGonzalez/spss-linux-launcher/main/spss.png
+SAV_FILE=https://raw.githubusercontent.com/JAlbertoGonzalez/spss-linux-launcher/main/application-x-spss-sav.png
+MIME_FILE=https://raw.githubusercontent.com/JAlbertoGonzalez/spss-linux-launcher/main/spss-mime.xml
 
 curl $DESKTOP_FILE -o spss.desktop
 curl $ICON_FILE -o spss.png
+curl $SAV_FILE -o application-x-spss-sav.png
+curl $MIME_FILE -o spss-mime.xml
 
 SPSS_EXISTS=${SPSS_EXISTS[0]}
 sed -i "s|Exec=.*|Exec=$SPSS_EXISTS %u|" spss.desktop
@@ -32,3 +36,8 @@ cat spss.desktop
 
 sudo mv ./spss.png /usr/share/pixmaps/
 sudo mv ./spss.desktop /usr/share/application
+sudo mv ./application-x-spss-sav.png /usr/share/icons/hicolor/scalable/mimetypes
+
+xdg-mime install spss-mime.xml
+sudo update-mime-database /usr/share/mime
+sudo update-icon-caches /usr/share/icons/*
